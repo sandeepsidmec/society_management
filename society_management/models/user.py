cf06388981e944fb8dd828072873bb8f66cff733
akshaya-sidmec <akshaya@sidmectech.in>
2025-05-08 11:36:12 +0530

    ADDED

---
+++ society_management/models/user.py
@@ -0,0 +1,14 @@
+
+from odoo import models, fields
+
+class User(models.Model):
+    _name = 'society.user'
+    _description = 'Society_User'
+
+    name= fields.Many2one("res.users",string=" Full Name", required=True)
+    email= fields.Char(string=" Email_address", required=True)
+    phone= fields.Integer(string=" Phone Number")
+    role= fields.Char(string="Role")
+    status= fields.Selection([('active', 'Active'), ('inactive', 'Inactive')],string="Status")
+    photo=fields.Binary("photo_upload")
+
